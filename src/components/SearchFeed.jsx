eeac37628918cb11a6d6829e4f2cc11512709c2e
abdelazeem492 <111613438+abdelazeem492@users.noreply.github.com>
2023-02-11 16:36:22 +0200

    Add files via upload

---
+++ src/components/SearchFeed.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+import { Videos } from "./";
+
+const SearchFeed = () => {
+  const [videos, setVideos] = useState([]);
+
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) => {
+      setVideos(data.items);
+    });
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "92vh", flex: 2 }}>
+      <Typography
+        variant="h4"
+        fontWeight={"bold"}
+        mb={2}
+        sx={{ color: "white" }}
+      >
+        Search Results For :{" "}
+        <span style={{ color: "#f31503" }}>{searchTerm}</span> Videos
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
+};
+
+export default SearchFeed;
